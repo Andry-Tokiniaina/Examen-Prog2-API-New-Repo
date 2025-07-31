7668b8cc72e98107ea7bad9a1f9d8ad1ef86157f
Toky <tokiniainarakotozanany@gmail.com>
2025-07-31 08:50:18 +0300

    première proposition de réponse avant vérification

--- main.py
+++ main.py
@@ -3,10 +3,49 @@ from pydantic import BaseModel
 from typing import List
 from starlette.requests import Request
 from starlette.responses import Response, JSONResponse
+import datetime
 
 app = FastAPI()
 
 
-@app.get("/")
+class PostModel(BaseModel):
+    author : str
+    title : str
+    content : str
+    creation_datetime : datetime.datetime
+
+post_list : List[PostModel] = []
+
+@app.get("/ping")
 def root():
-    return Response(content="Hello world!", status_code=200, media_type="text/plain")
+    return Response(content="pong", status_code=200, media_type="text/plain")
+
+@app.get("/home")
+def home():
+    return Response(content="<h1>Welcome home!</h1>", status_code=200, media_type="text/html")
+
+@app.post("/posts")
+def new_post(posts : List[PostModel]):
+    post_list.extend(PostModel.model_validate(post) for post in posts)
+    return Response(content={"posts":[post.model_dump() for post in post_list]}, status_code=200, media_type="application/json")
+
+@app.get("/posts")
+def get_posts():
+    return Response(content=[post_list], status_code=200, media_type="text/plain")
+
+@app.put("/posts")
+def update_posts(posts: List[PostModel]):
+    global post_list
+    for new_post in posts:
+        found = False
+        for i, existing_post in enumerate(post_list):
+            if new_post.title == existing_post.title:
+                post_list[i] = new_post
+                found = True
+                break
+        if not found:
+            post_list.append(new_post)
+
+@app.get("/{full_path:path}")
+def catch_all(full_path: str):
+    return Response(content="<p>404 not found</p>", status_code=404, media_type="text/html")
